d2c6d2d277b9e22cd2d558006572c17c77c90b6f
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2025-07-28 10:27:02 +0700

    second comit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.tsx
             </code>
-            .
+            . 
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
